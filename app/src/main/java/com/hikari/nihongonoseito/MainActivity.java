a41d5000f596e6086495fd1d5381f2164f77c46f
GustavoEliseu <30469845+GustavoEliseu@users.noreply.github.com>
2019-04-11 19:57:11 -0300

    Tela Vocabulário Placeholders
    Implementadas as funções da tela vocabulário, com placeholders e um "design" temporário.

--- app/src/main/java/com/hikari/nihongonoseito/MainActivity.java
+++ app/src/main/java/com/hikari/nihongonoseito/MainActivity.java
@@ -1,13 +1,39 @@
 package com.hikari.nihongonoseito;
 
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements myRecyclerAd.ItemClickListener {
+    myRecyclerAd mRecyclerAd;
+    RecyclerView mRecycler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mRecycler= findViewById(R.id.myRecycler);
+        mRecycler.setLayoutManager(new LinearLayoutManager(this));
+        mRecyclerAd = new myRecyclerAd(this,getResources().getStringArray(R.array.listaVocab),getResources().getStringArray(R.array.vocabImgs),getResources().getStringArray(R.array.listaVocabTraduz));
+        mRecyclerAd.setClickListener(this);
+        mRecycler.setAdapter(mRecyclerAd);
+        mRecyclerAd.notifyItemChanged(0);
+
+    }
+
+    @Override
+    public void onItemClick(View view, int position, Boolean tipo) {
+        String teste = "";
+        if(tipo == true) teste = "botao pequeno "+ position;
+        else teste = "vocab na posicao "+ position;
+        Log.v("TAGClicked","meu numero foi"+ position);
+        Toast.makeText(this,teste,Toast.LENGTH_SHORT).show();
     }
 }
---
+++ app/src/main/java/com/hikari/nihongonoseito/myRecyclerAd.java
@@ -0,0 +1,114 @@
+package com.hikari.nihongonoseito;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class myRecyclerAd extends RecyclerView.Adapter<myRecyclerAd.ViewHolder> {
+    private ItemClickListener mClickListener;
+    private String[] mData,mvVocabsImg, mDataTrad;
+    private Context context;
+    private LayoutInflater mInflater;
+
+    public myRecyclerAd(Context context,String data[], String[] vocabsImg,String[] mDataTrad){
+        this.mInflater = LayoutInflater.from(context);
+        mData = data;
+        this.mDataTrad=mDataTrad;
+        mvVocabsImg=vocabsImg;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public myRecyclerAd.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View view = mInflater.inflate(R.layout.recycler_view_item, viewGroup,false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
+        viewHolder.mTitleView.setText(mData[i]);
+        viewHolder.vocabTraduz.setText(mDataTrad[i]);
+        //viewHolder.imageVocab.setImageDrawable( context.getResources().getDrawable(context.getResources().getIdentifier(mvVocabsImg[i], "drawable", context.getPackageName())));
+        viewHolder.imageVocab.setImageDrawable(context.getResources().getDrawable(R.drawable.food_placeholder));
+        viewHolder.imageMarcarVocab.setImageDrawable(context.getResources().getDrawable(R.drawable.btn_cancelar_placeholder));
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mData.length;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+        // each data item is just a string in this case
+        public TextView mTitleView, vocabTraduz;
+        public ImageView imageVocab,imageMarcarVocab;
+        public boolean controleCor = true;
+
+        public ViewHolder(View v) {
+            super(v);
+            mTitleView = v.findViewById(R.id.vocabTitulo);
+            vocabTraduz = v.findViewById(R.id.vocabTraduz);
+            imageVocab = v.findViewById(R.id.imageVocab);
+            imageMarcarVocab = v.findViewById(R.id.imageMarcarVocab);
+            v.setOnClickListener(this);
+            imageMarcarVocab.setOnClickListener(this);
+
+        }
+
+        public void inverteCor(){
+            this.controleCor = !controleCor;
+            if(controleCor==true){
+                mTitleView.setTextColor(Color.BLACK);
+            } else{
+                mTitleView.setTextColor(Color.GRAY);
+            }
+
+        }
+
+
+        @Override
+        public void onClick(View v) {
+            if (mClickListener != null)
+                if(v.getId()==imageMarcarVocab.getId()){//click no botão cancelar
+                    inverteCor();
+                    /*
+                    * Se necessário, pode utilizar a boolean controlecor, como algo para saber se o item está habilitado ou não.
+                    * com if(controleCor==true).
+                    * */
+                    mClickListener.onItemClick(imageMarcarVocab, getAdapterPosition(),true);
+                }
+                else{//click no view em si
+
+                    mClickListener.onItemClick(v, getAdapterPosition(),false);
+                }
+        }
+    }
+
+    // convenience method for getting data at click position
+    String getItem(int id) {
+        return mData[id];
+    }
+
+    // allows clicks events to be caught
+    void setClickListener(ItemClickListener itemClickListener) {
+        this.mClickListener = itemClickListener;
+    }
+
+
+    public interface ItemClickListener {
+        void onItemClick(View view, int position, Boolean tipoClasse);
+    }
+}
+
+
+
+
