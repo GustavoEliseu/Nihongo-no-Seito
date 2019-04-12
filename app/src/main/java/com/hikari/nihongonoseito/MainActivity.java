8f83d0976c5ea001d28b818426bde0d281b06d27
GustavoEliseu <30469845+GustavoEliseu@users.noreply.github.com>
2019-04-12 00:20:42 -0300

    Telas design
    Tentativa de protótipo das telas de design, como as mesmas ainda não estavam protas o gabriel fez estas e me passou para tentar compensar.

--- app/src/main/java/com/hikari/nihongonoseito/MainActivity.java
+++ app/src/main/java/com/hikari/nihongonoseito/MainActivity.java
@@ -1,39 +1,145 @@
 package com.hikari.nihongonoseito;
 
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.app.ActionBarDrawerToggle;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements myRecyclerAd.ItemClickListener {
-    myRecyclerAd mRecyclerAd;
-    RecyclerView mRecycler;
+//TODO- IMPLEMENTAR DRAWER COMO FRAGMENT!
+public class MainActivity extends AppCompatActivity  implements NavigationView.OnNavigationItemSelectedListener, Quiz_Fragment.OnFragmentInteractionListener,
+        Vocab_Fragment.OnFragmentInteractionListener,Kanji_Fragment.OnFragmentInteractionListener{
+
+    NavigationView navigationView;
+    Fragment mFrag;
+
+    private  DrawerLayout drawer;
+    private ActionBarDrawerToggle toggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
         setContentView(R.layout.activity_main);
-        mRecycler= findViewById(R.id.myRecycler);
-        mRecycler.setLayoutManager(new LinearLayoutManager(this));
-        mRecyclerAd = new myRecyclerAd(this,getResources().getStringArray(R.array.listaVocab),getResources().getStringArray(R.array.vocabImgs),getResources().getStringArray(R.array.listaVocabTraduz));
-        mRecyclerAd.setClickListener(this);
-        mRecycler.setAdapter(mRecyclerAd);
-        mRecyclerAd.notifyItemChanged(0);
+
+
+        //definindo o Toolbar
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        //Configuração do menu lateral e inicialização deste
+        drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+
+        toggle.syncState();
+
+
+    }
+    @Override
+    public void onStart(){
+        super.onStart();
+        int idChecked = R.id.nav_vocab;
+
+
+
+        navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setItemIconTintList(null);
+        navigationView.setNavigationItemSelectedListener(this);
+        navigationView.setCheckedItem(idChecked);
+
+        changeFragment(idChecked);
 
     }
 
+    public void changeFragment(int checkedId){
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        removeFragment();
+        switch(checkedId){
+            case R.id.nav_vocab:
+                mFrag= new Vocab_Fragment();
+                ft.replace(R.id.frag_placeholder, mFrag);
+                break;
+            case R.id.nav_kanji:
+                mFrag= new Kanji_Fragment();
+                ft.replace(R.id.frag_placeholder, mFrag);
+                break;
+            case R.id.nav_quiz:
+                mFrag= new Quiz_Fragment();
+                ft.replace(R.id.frag_placeholder, mFrag);
+                break;
+        }
+        ft.commit();
+    }
+    public void removeFragment(){
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        if (mFrag != null) {
+            ft.remove(mFrag);
+            ft.commit();
+            ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_CLOSE);
+            mFrag = null;
+        }
+    }
+
     @Override
-    public void onItemClick(View view, int position, Boolean tipo) {
-        String teste = "";
-        if(tipo == true) teste = "botao pequeno "+ position;
-        else teste = "vocab na posicao "+ position;
-        Log.v("TAGClicked","meu numero foi"+ position);
-        Toast.makeText(this,teste,Toast.LENGTH_SHORT).show();
+    public void onStop(){
+        int idChecked = R.id.nav_vocab;
+        navigationView.setCheckedItem(idChecked);
+        super.onStop();
+
+    }
+
+
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        int id =menuItem.getItemId();
+
+        switch(id){
+            case R.id.nav_vocab:
+               changeFragment(id);
+                break;
+            case R.id.nav_kanji:
+                changeFragment(id);
+                break;
+            case R.id.nav_quiz:
+                changeFragment(id);
+                break;
+        }
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+
+    @Override
+    public void onFragmentInteraction(String tag, Object data) {
+
     }
 }
